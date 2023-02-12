67f4ddb4266ce476d793a8caf21d5cd4e541d334
scarbrse <122652254+scarbrse@users.noreply.github.com>
2023-02-12 13:45:17 -0500

    setInterval and call updateMessgaes

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,6 +3,7 @@ const myMessage = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
 const chatBox = document.getElementById("chat");
 const serverURL = 'https://it3049c-chat-application.herokuapp.com/messages';
+const MILLISECONDS_IN_TEN_SECONDS = 10000;
 
 function updateMessages() {
   this.fetchMessages();
@@ -16,6 +17,37 @@ function fetchMessages() {
   .then(response => response.json())
 };
 
+
+
+function formatMessage(message, myNameInput) {
+  const time = new Date(message.timestamp);
+  const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
+
+  if (myNameInput === message.sender) {
+      return `
+      <div class="mine messages">
+          <div class="message">
+              ${message.text}
+          </div>
+          <div class="sender-info">
+              ${formattedTime}
+          </div>
+      </div>
+      `
+  } else {
+      return `
+          <div class="yours messages">
+              <div class="message">
+                  ${message.text}
+              </div>
+              <div class="sender-info">
+                  ${message.sender} ${formattedTime}
+              </div>
+          </div>
+      `
+  };
+};
+
 async function updateMessages() {
   const messages = await fetchMessages();
   console.log(messages);
@@ -25,4 +57,7 @@ async function updateMessages() {
   });
   chatBox.innerHTML = formatMessages;
 
-}
+};
+
+updateMessages()
+setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
