1163e38380978e13f7c12f678b0ee39932f495bd
scarbrse <122652254+scarbrse@users.noreply.github.com>
2023-02-16 14:57:03 -0500

    update serverURL

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,7 +2,7 @@ const nameInput = document.getElementById("my-name-input");
 const myMessage = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
 const chatBox = document.getElementById("chat");
-const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
+const serverURL = `https://it3049c-chat.fly.dev/messages`;
 const MILLISECONDS_IN_TEN_SECONDS = 10000;
 
 
